2c4e8c7b581ccfa2ca19908ae68bd9e8dbc9b18c
Kevin Naquin <kevinnaquin@Kevins-MacBook-Pro.local>
2016-01-21 12:15:28 -0500

    Trying it via command-line

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,5 +11,5 @@ source $base/vars.sh
 cd coord-convert
 grails compile
 #grails -Dbuild.compiler=javac1.7 build-standalone $ARTIFACT
-grails war $ARTIFACT
-aws s3 cp $ARTIFACT s3://$S3BUCKET/$APP/$ARTIFACT
+#grails war $ARTIFACT
+#aws s3 cp $ARTIFACT s3://$S3BUCKET/$APP/$ARTIFACT
--- scripts/cf-deliver.sh
+++ scripts/cf-deliver.sh
@@ -8,6 +8,6 @@ popd > /dev/null
 
 # gather some data about the repo
 source $base/vars.sh
-
+echo $PORT
 # do we have this artifact in s3? If not, fail.
-[ -f $base/../pzsvc-coordinate-conversion.war ] || { aws s3 ls $S3URL && aws s3 cp $S3URL $base/../pzsvc-coordinate-conversion.war || exit 1; }
+#[ -f $base/../pzsvc-coordinate-conversion.war ] || { aws s3 ls $S3URL && aws s3 cp $S3URL $base/../pzsvc-coordinate-conversion.war || exit 1; }
