10fb60dee4a3f5b245689532d1555b9bf66b5e37
Kevin Naquin <kevinnaquin@Kevins-MacBook-Pro.local>
2016-01-21 12:25:34 -0500

    Testing

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ popd > /dev/null
 source $base/vars.sh
 
 cd coord-convert
-grails compile
+#grails compile
 #grails -Dbuild.compiler=javac1.7 build-standalone $ARTIFACT
 #grails war $ARTIFACT
 #aws s3 cp $ARTIFACT s3://$S3BUCKET/$APP/$ARTIFACT
