fa31d290c743d117434b2d25d072cf5aebcabc86
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-03-28 08:48:52 -0400

    Seeing what OS the build is on.

--- ci/archive.sh
+++ ci/archive.sh
@@ -6,7 +6,7 @@ popd > /dev/null
 
 # gather some data about the repo
 source $root/ci/vars.sh
-
+uname -a
 ! type grails >/dev/null 2>&1 && source $root/ci/grails.sh
 
 pushd $root/coord-convert > /dev/null
